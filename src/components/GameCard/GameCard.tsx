f40647b1390d50cd97ce7e45ca7f796338c23c18
DiguyaDeveloper <diegocecconcwb@outlook.com>
2022-09-21 21:29:42 -0300

    feat: :sparkles: Create game card component

---
+++ src/components/GameCard/GameCard.tsx
@@ -0,0 +1,27 @@
+import { LinearGradient } from "expo-linear-gradient";
+import {
+  Text,
+  ImageBackground,
+  TouchableOpacity,
+  TouchableOpacityProps,
+} from "react-native";
+import { GameCardProps } from "../../shared/interfaces/game-card.interface";
+import { THEME } from "../../theme";
+import { styles } from "./GameCard.styles";
+
+interface Props extends TouchableOpacityProps {
+  data: GameCardProps;
+}
+
+export function GameCard({ data, ...rest }: Props) {
+  return (
+    <TouchableOpacity style={styles.container} {...rest}>
+      <ImageBackground style={styles.cover} source={data.cover}>
+        <LinearGradient colors={THEME.COLORS.FOOTER} style={styles.footer}>
+          <Text style={styles.name}>{data.name}</Text>
+          <Text style={styles.ads}>{data.ads} anúncios</Text>
+        </LinearGradient>
+      </ImageBackground>
+    </TouchableOpacity>
+  );
+}
