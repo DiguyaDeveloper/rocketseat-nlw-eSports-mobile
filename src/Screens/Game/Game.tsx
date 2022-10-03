7259d2f55b2750ad1e181fdcb62d7f1708391507
DiguyaDeveloper <diegocecconcwb@outlook.com>
2022-10-03 20:26:51 -0300

    feat(aula-4): :sparkles: Update GameScreen with params and route

--- src/Screens/Game/Game.tsx
+++ src/Screens/Game/Game.tsx
@@ -1,7 +1,16 @@
 import { styles } from "./Game.styles";
-
+import { useRoute } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Background } from "../../components/Background/Background";
+import { GameParams } from "../../shared/interfaces/game-params.interface";
 
 export function GameScreen() {
-  return <SafeAreaView></SafeAreaView>;
+  const route = useRoute();
+  const game: GameParams = route.params as GameParams;
+
+  return (
+    <Background>
+      <SafeAreaView></SafeAreaView>
+    </Background>
+  );
 }
