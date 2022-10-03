56c5fdbf7b604acd9fcecba37135830d2e4a7df4
DiguyaDeveloper <diegocecconcwb@outlook.com>
2022-10-03 20:46:09 -0300

    feat(aula-4): :sparkles: Increment GameScreen component
    Use styles of Game Screen
    Introduce goBack function on react navigate
    Introduce Image banner of game
    Introduce get data game in path params
    Introduce Heading of page
    Introduce DuoCard
    Introduce Icon of Entypo expo
    Introduce Logo and view without content to align items

--- src/Screens/Game/Game.tsx
+++ src/Screens/Game/Game.tsx
@@ -1,16 +1,45 @@
+import { View, TouchableOpacity, Image } from "react-native";
+import { Entypo } from "@expo/vector-icons";
 import { styles } from "./Game.styles";
-import { useRoute } from "@react-navigation/native";
+import { useRoute, useNavigation } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Background } from "../../components/Background/Background";
 import { GameParams } from "../../shared/interfaces/game-params.interface";
+import { THEME } from "../../theme";
+import logoImg from "../../assets/logo-nlw-esports.png";
+import { Heading } from "../../components/Heading/Heading";
+import { DuoCard } from "../../components/DuoCard/DuoCard";
 
 export function GameScreen() {
+  const navigation = useNavigation();
   const route = useRoute();
   const game: GameParams = route.params as GameParams;
 
+  const handleGoBack = () => {
+    navigation.goBack();
+  };
+
   return (
     <Background>
-      <SafeAreaView></SafeAreaView>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={handleGoBack}>
+            <Entypo name="chevron-thin-left" color={THEME.COLORS.CAPTION_300} />
+            <Image source={logoImg} style={styles.logo} />
+            <View style={styles.right}></View>
+          </TouchableOpacity>
+        </View>
+
+        <Image
+          source={{ uri: game.bannerUrl }}
+          style={styles.cover}
+          resizeMode="cover"
+        />
+
+        <Heading title={game.title} subtitle="Conecte-se e comece a jogar!" />
+
+        <DuoCard />
+      </SafeAreaView>
     </Background>
   );
 }
