d55d382729c45419f6c35382ef2a1779ce861c37
DiguyaDeveloper <diegocecconcwb@outlook.com>
2022-10-06 22:05:11 -0300

    feat(aula-5): :sparkles: Created loading on copy
    Created alert on copy

--- src/components/DuoMatch/DuoMatch.tsx
+++ src/components/DuoMatch/DuoMatch.tsx
@@ -1,9 +1,19 @@
-import { View, Modal, ModalProps, Text, TouchableOpacity } from "react-native";
+import {
+  View,
+  Modal,
+  ModalProps,
+  Text,
+  TouchableOpacity,
+  Alert,
+  ActivityIndicator,
+} from "react-native";
 import { styles } from "./DuoMatch.styles";
 import { MaterialIcons } from "@expo/vector-icons";
 import { CheckCircle } from "phosphor-react-native";
 import { THEME } from "../../theme";
 import { Heading } from "../Heading/Heading";
+import * as Clipboard from "expo-clipboard";
+import { useState } from "react";
 
 interface DuoMatchProps extends ModalProps {
   discord: string;
@@ -11,6 +21,18 @@ interface DuoMatchProps extends ModalProps {
 }
 
 export function DuoMatch({ discord, onClose, ...rest }: DuoMatchProps) {
+  const [isCopping, setIsCopping] = useState<boolean>(false);
+
+  async function handleCopyDiscordClipboard() {
+    setIsCopping(true);
+    await Clipboard.setStringAsync(discord);
+    setIsCopping(false);
+    Alert.alert(
+      "Discord copiado!",
+      "Usuário copiado para você colocar no Discord e encontrar a pessoa!"
+    );
+  }
+
   return (
     <Modal {...rest} transparent statusBarTranslucent animationType="fade">
       <View style={styles.container}>
@@ -37,8 +59,18 @@ export function DuoMatch({ discord, onClose, ...rest }: DuoMatchProps) {
 
           <Text style={styles.label}>Adicione no Discord</Text>
 
-          <TouchableOpacity style={styles.discordButton}>
-            <Text style={styles.discord}>{discord}</Text>
+          <TouchableOpacity
+            style={styles.discordButton}
+            onPress={handleCopyDiscordClipboard}
+            disabled={isCopping}
+          >
+            <Text style={styles.discord}>
+              {isCopping ? (
+                <ActivityIndicator color={THEME.COLORS.PRIMARY} />
+              ) : (
+                discord
+              )}
+            </Text>
           </TouchableOpacity>
         </View>
       </View>
